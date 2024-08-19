df439df689b116b43cafb94ac88d233fa1bce1b1
YenJui <155418322+YenJui@users.noreply.github.com>
2024-08-19 10:27:54 +0800

    1.1.5 (#19) (#20)
    * Initial push
    
    Initial push
    
    * Minor Fix
    
    .env.example changes
    
    * Change to Line Redirect (.env)
    
    * Business User
    
    * User routes updates
    
    * Auth changes and contact us
    
    * Hide password on user object
    
    * 1.1.0
    
    * 1.1.1
    
    * 1.1.2
    
    - Bind to Facebook
    - Statistics
    
    * 1.1.3
    
    - Changes in validation errorlist
    - Changes in unauthorized HTTP Code 401 Unauthorised
    - Addition o N5 badges
    
    * 1.1.4
    
    - Breathing points
    - OOSA daily award breathing point
    
    * 1.1.5
    
    - User badges - Change structure
    
    Co-authored-by: fortunio <10980185+fortunio@users.noreply.github.com>

---
+++ internal/helpers/notifications.go
@@ -0,0 +1,72 @@
+package helpers
+
+import (
+	"context"
+	"oosa/internal/config"
+	"oosa/internal/models"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var (
+	NOTIFICATION_INVITATION              = "INVITATION"
+	NOTIFICATION_JOINING_NEW             = "JOINING_NEW"
+	NOTIFICATION_JOINING_REQUEST         = "JOINING_REQUEST"
+	NOTIFICATION_FRIEND_REQUEST          = "FRIEND_REQUEST"
+	NOTIFICATION_FRIEND_REQUEST_ACCEPTED = "FRIEND_REQUEST_ACCEPTED"
+	NOTIFICATION_BADGE_NEW               = "BADGE_NEW"
+	NOTIFICATION_STARMAP_NEW             = "STARMAP_NEW"
+	NOTIFICATION_STARMAP_PROGRESS        = "STARMAP_PROGRESS"
+	NOTIFICATION_UPDATE_POLICY           = "UPDATE_POLICY"
+	NOTIFICATION_EVENT_INFO              = "EVENT_INFO"
+	NOTIFICATION_EVENT_COUNTDOWN         = "EVENT_COUNTDOWN"
+	NOTIFICATION_EVENT_MEMBER_DELETED    = "EVENT_MEMBER_DELETED"
+	NOTIFICATION_EVENT_JOIN_DENIED       = "EVENT_JOIN_DENIED"
+	NOTIFICATION_COLOG_PHOTO_UPLOADED    = "COLOG_PHOTO_UPLOADED"
+	NOTIFICATION_COLOG_REMIND            = "COLOG_REMIND"
+)
+
+func NotificationsCreate(c *gin.Context, notifCode string, userId primitive.ObjectID, message models.NotificationMessage, identifier primitive.ObjectID) {
+	userDetail := GetAuthUser(c)
+	insert := models.Notifications{
+		NotificationsCode:       notifCode,
+		NotificationsUser:       userId,
+		NotificationsMessage:    message,
+		NotificationsIdentifier: identifier,
+		NotificationsCreatedAt:  primitive.NewDateTimeFromTime(time.Now()),
+		NotificationsCreatedBy:  userDetail.UsersId,
+	}
+	config.DB.Collection("Notifications").InsertOne(context.TODO(), insert)
+}
+
+func NotificationFormatEvent(Events models.Events) map[string]any {
+	return map[string]any{
+		"events_id":   Events.EventsId,
+		"events_name": Events.EventsName,
+	}
+}
+
+func NotificationFormatUser(Users models.Users) map[string]any {
+	return map[string]any{
+		"users_id":   Users.UsersId,
+		"users_name": Users.UsersName,
+	}
+}
+
+func NotificationFormatUserFollowing(UserFollowings models.UserFollowings) map[string]any {
+	return map[string]any{
+		"user_followings_id":        UserFollowings.UserFollowingsId,
+		"user_followings_user":      UserFollowings.UserFollowingsUser,
+		"user_followings_following": UserFollowings.UserFollowingsFollowing,
+	}
+}
+
+func NotificationFormatBadges(Badges models.Badges) map[string]any {
+	return map[string]any{
+		"badges_id":   Badges.BadgesId,
+		"badges_code": Badges.BadgesCode,
+		"badges_name": Badges.BadgesName,
+	}
+}
