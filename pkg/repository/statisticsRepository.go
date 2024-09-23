c281c7a2dfe43851e0a04f41e9ffcc8ffd2e8773
fortunio <10980185+fortunio@users.noreply.github.com>
2024-09-23 10:23:26 +0700

    1.1.9 (#24)
    * Initial push
    
    Initial push
    
    * Minor Fix
    
    .env.example changes
    
    * Change to Line Redirect (.env)
    
    * Business User
    
    * User routes updates
    
    * Auth changes and contact us
    
    * Hide password on user object
    
    * 1.1.0
    
    * 1.1.1
    
    * 1.1.2
    
    - Bind to Facebook
    - Statistics
    
    * 1.1.3
    
    - Changes in validation errorlist
    - Changes in unauthorized HTTP Code 401 Unauthorised
    - Addition o N5 badges
    
    * 1.1.4
    
    - Breathing points
    - OOSA daily award breathing point
    
    * 1.1.5
    
    - User badges - Change structure
    
    * 1.1.6
    
    - Get OOSA users 3.3.2.1 ~ 3.3.2.1c, 3.1.1.4 ~ 3.1.1.4a
    
    * 1.1.7
    
    - _O1
    -  _O1, _O1a, _O1b, _O1c - Get user detail and badges
    - 4.2e ~ 4.2h - Tab4 Friends
    
    * 1.1.8
    
    4.0 - World Statistics
    
    * 1.1.9
    
    - 4.3 - Top 5 Ranking (Feelings/Rewild)
    
    * 1.1.10
    
    - 4, 4.1, 4.3 - Removal of Auth Guards

--- pkg/repository/statisticsRepository.go
+++ pkg/repository/statisticsRepository.go
@@ -96,3 +96,120 @@ func (t StatisticsRepository) Retrieve(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (t StatisticsRepository) RetrieveRankingFeelings(c *gin.Context) {
+	var RewildingRankingFeelings []models.RewildingRanking
+	eventParticipantsAgg := t.GetRankingPipeline(c, 1)
+	eventParticipantsCursor, eventParticipantsErr := config.DB.Collection("EventParticipants").Aggregate(context.TODO(), eventParticipantsAgg)
+	eventParticipantsCursor.All(context.TODO(), &RewildingRankingFeelings)
+
+	if eventParticipantsErr != nil {
+		helpers.ResponseError(c, eventParticipantsErr.Error())
+		return
+	}
+
+	if len(RewildingRankingFeelings) == 0 {
+		helpers.ResponseNoData(c, "No Data")
+		return
+	}
+	c.JSON(200, RewildingRankingFeelings)
+}
+
+func (t StatisticsRepository) RetrieveRankingRewilding(c *gin.Context) {
+	var RewildingRankingFeelings []models.RewildingRanking
+	eventParticipantsAgg := t.GetRankingPipeline(c, 2)
+	eventParticipantsCursor, eventParticipantsErr := config.DB.Collection("EventParticipants").Aggregate(context.TODO(), eventParticipantsAgg)
+	eventParticipantsCursor.All(context.TODO(), &RewildingRankingFeelings)
+
+	if eventParticipantsErr != nil {
+		helpers.ResponseError(c, eventParticipantsErr.Error())
+		return
+	}
+
+	if len(RewildingRankingFeelings) == 0 {
+		helpers.ResponseNoData(c, "No Data")
+		return
+	}
+	c.JSON(200, RewildingRankingFeelings)
+}
+
+func (t StatisticsRepository) GetRankingPipeline(c *gin.Context, groupType int) mongo.Pipeline {
+	// 1: Ranking based on feelings, 2: Ranking based on participant count
+	rankingFeelings := c.Query("feelings")
+	pipeline := mongo.Pipeline{
+		bson.D{{
+			Key: "$match", Value: bson.M{
+				"event_participants_experience": bson.M{"$exists": true, "$eq": rankingFeelings},
+			},
+		}},
+		bson.D{{
+			Key: "$lookup", Value: bson.M{
+				"from":         "Events",
+				"localField":   "event_participants_event",
+				"foreignField": "_id",
+				"as":           "event_participants_event_detail",
+			},
+		}},
+		bson.D{{
+			Key: "$unwind", Value: "$event_participants_event_detail",
+		}},
+		bson.D{{
+			Key: "$replaceRoot", Value: bson.M{
+				"newRoot": "$event_participants_event_detail",
+			},
+		}},
+		bson.D{{
+			Key: "$lookup", Value: bson.M{
+				"from":         "RefRewildingTypes",
+				"localField":   "events_type",
+				"foreignField": "_id",
+				"as":           "events_type_detail",
+			},
+		}},
+		bson.D{{
+			Key: "$unwind", Value: bson.M{"path": "$events_type_detail", "preserveNullAndEmptyArrays": true},
+		}},
+		bson.D{{
+			Key: "$group", Value: bson.D{
+				{Key: "_id", Value: "$events_rewilding"},
+				{Key: "rewilding_participants_experience_count", Value: bson.M{"$sum": 1}},
+				{Key: "rewilding_type_list", Value: bson.M{"$addToSet": "$events_type_detail.ref_rewilding_types_name"}},
+			},
+		}},
+		bson.D{{
+			Key: "$match", Value: bson.M{
+				"rewilding_participants_experience_count": bson.M{"$gte": config.APP_LIMIT.MinimumTopRanking},
+			},
+		}},
+		bson.D{{
+			Key: "$lookup", Value: bson.M{
+				"from":         "Rewilding",
+				"localField":   "_id",
+				"foreignField": "_id",
+				"as":           "events_rewilding_detail",
+			},
+		}},
+		bson.D{{
+			Key: "$unwind", Value: "$events_rewilding_detail",
+		}},
+		bson.D{{
+			Key: "$replaceRoot", Value: bson.M{
+				"newRoot": bson.M{
+					"$mergeObjects": bson.A{
+						"$events_rewilding_detail",
+						bson.M{"rewilding_type_list": "$rewilding_type_list"},
+						bson.M{"rewilding_participants_experience_count": "$rewilding_participants_experience_count"},
+					},
+				},
+			},
+		}},
+		bson.D{{Key: "$sort", Value: bson.M{"rewilding_participants_experience_count": -1}}},
+		bson.D{{Key: "$limit", Value: 5}},
+	}
+
+	if groupType == 2 {
+		_, pipeline = pipeline[0], pipeline[1:]
+	}
+
+	return pipeline
+}
--- routes/worldRoutes.go
+++ routes/worldRoutes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"oosa/internal/middleware"
 	"oosa/pkg/repository"
 
 	"github.com/gin-gonic/gin"
@@ -9,9 +8,11 @@ import (
 
 func WorldRoutes(r *gin.Engine) *gin.Engine {
 	repository := repository.StatisticsRepository{}
-	main := r.Group("/world", middleware.AuthMiddleware())
+	main := r.Group("/world")
 	{
 		main.GET("statistics", repository.Retrieve)
+		main.GET("ranking-feelings", repository.RetrieveRankingFeelings)
+		main.GET("ranking-rewilding", repository.RetrieveRankingRewilding)
 	}
 
 	return r
