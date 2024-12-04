abab1206aa4033c0e92e9e144bb7f9fe26218561
fortunio <10980185+fortunio@users.noreply.github.com>
2024-12-04 13:48:11 +1100

    1.1.21 (#37)
    * Initial push
    
    Initial push
    
    * Minor Fix
    
    .env.example changes
    
    * Change to Line Redirect (.env)
    
    * Business User
    
    * User routes updates
    
    * Auth changes and contact us
    
    * Hide password on user object
    
    * 1.1.0
    
    * 1.1.1
    
    * 1.1.2
    
    - Bind to Facebook
    - Statistics
    
    * 1.1.3
    
    - Changes in validation errorlist
    - Changes in unauthorized HTTP Code 401 Unauthorised
    - Addition o N5 badges
    
    * 1.1.4
    
    - Breathing points
    - OOSA daily award breathing point
    
    * 1.1.5
    
    - User badges - Change structure
    
    * 1.1.6
    
    - Get OOSA users 3.3.2.1 ~ 3.3.2.1c, 3.1.1.4 ~ 3.1.1.4a
    
    * 1.1.7
    
    - _O1
    -  _O1, _O1a, _O1b, _O1c - Get user detail and badges
    - 4.2e ~ 4.2h - Tab4 Friends
    
    * 1.1.8
    
    4.0 - World Statistics
    
    * 1.1.9
    
    - 4.3 - Top 5 Ranking (Feelings/Rewild)
    
    * 1.1.10
    
    - 4, 4.1, 4.3 - Removal of Auth Guards
    
    * 1.1.11
    
    - GENERAL - user-following, auth routes remove trailing slashes
    
    * 1.1.12
    
    - GET user?name= Show
    - user-following routes to trigger count on delete, create and update
    
    * 1.1.13
    
    - world/ranking-feelings - default to EXPERIENCE_1, allow only EXPERIENCE_1 to EXPERIENCE_6
    
    * 1.1.14
    
    -  GET friends, POST friends, PUT friends/:userFriendId, DELETE friends/:userFriendId, /user/friends/recommended- Changes to friend
    
    * 1.1.15
    
    - GET users, GET users/statistics. Renamed 2 endpoints
    - GET user/{userId}/friends. Retrieve other user friends
    
    * 1.1.16
    
    - User friends recommended (officially recommended friends, people who went together in an event)
    - Auto accept friend request based on user setting
    
    * 1.1.17
    
    - User friends notification (PENDING, ACCEPTED)
    
    * 1.1.18
    
    - Username update regex, 60 days once allowed change
    - Password check regex: POST register, PUT auth/change-password, POST forget-password
    - User name check regex: POST register, PUT auth
    - Recommended friends: other users who are not friends
    
    * 1.1.19
    
    - Update regex change password
    
    * 1.1.20
    
    - Autoadd setting defaults to 0 when creating new account
    - Pending status shown in recommended friends
    - Filter recommended friends by name and username
    
    * 1.1.21
    
    - Fix bug on notification not sent on new user create
    
    * 1.1.22
    
    - Fix bug on incorect variable for searching non friends in recommended
    - User friends rewilding activity status
    - Update take me away status

--- internal/models/userFriends.go
+++ internal/models/userFriends.go
@@ -3,11 +3,12 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type UserFriends struct {
-	UserFriendsId         primitive.ObjectID `bson:"_id,omitempty" json:"user_friends_id"`
-	UserFriendsStatus     *int               `bson:"user_friends_status,omitempty" json:"user_friends_status"`
-	UserFriendsUser1      primitive.ObjectID `bson:"user_friends_user_1,omitempty" json:"user_friends_user_1"`
-	UserFriendsUser2      primitive.ObjectID `bson:"user_friends_user_2,omitempty" json:"user_friends_user_2"`
-	UserFriendsCreatedAt  primitive.DateTime `bson:"user_friends_created_at,omitempty" json:"user_friends_created_at"`
-	UserFriendsIsOfficial *bool              `bson:"user_friends_is_official,omitempty" json:"user_friends_is_official"`
-	UserFriendsDetail     *UsersAggBreathing `bson:"user_friends_detail,omitempty" json:"user_friends_detail,omitempty"`
+	UserFriendsId           primitive.ObjectID `bson:"_id,omitempty" json:"user_friends_id"`
+	UserFriendsStatus       *int               `bson:"user_friends_status,omitempty" json:"user_friends_status"`
+	UserFriendsUser1        primitive.ObjectID `bson:"user_friends_user_1,omitempty" json:"user_friends_user_1"`
+	UserFriendsUser2        primitive.ObjectID `bson:"user_friends_user_2,omitempty" json:"user_friends_user_2"`
+	UserFriendsCreatedAt    primitive.DateTime `bson:"user_friends_created_at,omitempty" json:"user_friends_created_at"`
+	UserFriendsIsOfficial   *bool              `bson:"user_friends_is_official,omitempty" json:"user_friends_is_official"`
+	RewildingActivityStatus int                `bson:"rewilding_activity_status,omitempty" json:"rewilding_activity_status,omitempty"`
+	UserFriendsDetail       *UsersAggBreathing `bson:"user_friends_detail,omitempty" json:"user_friends_detail,omitempty"`
 }
