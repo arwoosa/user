3f5ffffe7038aec1b2d725262b61faf0db432af2
Chen Yan Jui <ch.focke@gmail.com>
2025-01-11 20:16:16 +0800

    feat: health check close db connection

--- routes/health.go
+++ routes/health.go
@@ -23,14 +23,27 @@ func aliveHandler(c *gin.Context) {
 	})
 }
 
-func readyHandler(c *gin.Context) {
+var clientCache *mongo.Client
+
+func getClient(c *gin.Context) *mongo.Client {
+	if clientCache != nil {
+		return clientCache
+	}
 	client, err := mongo.Connect(c, options.Client().ApplyURI(config.APP.DbConnection))
 	if err != nil {
 		c.JSON(500, gin.H{"message": "Database is not ready, connect error: " + err.Error()})
-		return
+		return nil
 	}
-	err = client.Ping(c, nil)
+	clientCache = client
+	return clientCache
+}
+
+func readyHandler(c *gin.Context) {
+	client := getClient(c)
+	err := client.Ping(c, nil)
 	if err != nil {
+		_ = client.Disconnect(c)
+		clientCache = nil
 		c.JSON(500, gin.H{"message": "Database is not ready, ping error: " + err.Error()})
 		return
 	}
