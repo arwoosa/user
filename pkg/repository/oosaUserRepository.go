457eb4d7527ea1293ca222d4090b459a4420069d
AidChen0509 <as9135566@gmail.com>
2025-04-29 09:30:15 +0800

    Enhance user response in Read method to include friend status, friend count, and breathing points. Add tracking and friendship relationship information to the JSON response.

--- pkg/repository/oosaUserRepository.go
+++ pkg/repository/oosaUserRepository.go
@@ -28,6 +28,7 @@ func (r OosaUserRepository) Read(c *gin.Context) {
 		return
 	}
 
+	// 添加追蹤關係
 	var UserFollowing models.UserFollowings
 	UserFollowingFilter := bson.D{
 		{Key: "user_followings_user", Value: userDetail.UsersId},
@@ -38,7 +39,87 @@ func (r OosaUserRepository) Read(c *gin.Context) {
 		User.UsersFollowings = &UserFollowing
 	}
 
-	c.JSON(http.StatusOK, User)
+	// 添加好友關係信息
+	userFriendStatus := "stranger"
+	if userDetail.UsersId == User.UsersId {
+		userFriendStatus = "myself"
+	} else {
+		var UserFriends models.UserFriends
+		UserFriendsFilter := bson.D{
+			{Key: "$or", Value: []bson.D{
+				{
+					{Key: "user_friends_user_1", Value: userDetail.UsersId},
+					{Key: "user_friends_user_2", Value: User.UsersId},
+				},
+				{
+					{Key: "user_friends_user_1", Value: User.UsersId},
+					{Key: "user_friends_user_2", Value: userDetail.UsersId},
+				},
+			}},
+		}
+
+		UserFriendsErr := config.DB.Collection("UserFriends").FindOne(context.TODO(), UserFriendsFilter).Decode(&UserFriends)
+		if UserFriendsErr == nil {
+			// 將狀態碼轉換為文字狀態
+			// USER_RECOMMENDED = 0, USER_PENDING = 1, USER_ACCEPTED = 2, USER_CANCELLED = 3
+			switch *UserFriends.UserFriendsStatus {
+			case 2: // USER_ACCEPTED
+				userFriendStatus = "friend"
+			case 1: // USER_PENDING
+				// 判斷是誰發出的邀請
+				if (UserFriends.UserFriendsUser1 == userDetail.UsersId && UserFriends.UserFriendsUser2 == User.UsersId) ||
+					(UserFriends.UserFriendsUser2 == userDetail.UsersId && UserFriends.UserFriendsUser1 == User.UsersId) {
+					userFriendStatus = "invited"
+				}
+			default:
+				userFriendStatus = "stranger"
+			}
+		}
+	}
+
+	// 獲取好友數量
+	friendsCount := User.UsersFriendsCount
+
+	// 添加到響應中
+	usersFriendsInfo := map[string]interface{}{
+		"status": userFriendStatus,
+		"count":  friendsCount,
+	}
+
+	// 獲取用戶的 breathing points
+	breathingPoints := AuthRepository{}.Breathing(c, User.UsersId)
+
+	// 創建最終響應
+	response := map[string]interface{}{
+		"users_avatar":                              User.UsersAvatar,
+		"users_created_at":                          User.UsersCreatedAt,
+		"users_email":                               User.UsersEmail,
+		"users_id":                                  User.UsersId,
+		"users_is_business":                         User.UsersIsBusiness,
+		"users_is_subscribed":                       User.UsersIsSubscribed,
+		"users_name":                                User.UsersName,
+		"users_object":                              User.UsersObject,
+		"users_password":                            User.UsersPassword,
+		"users_setting_friend_auto_add":             User.UsersSettingFriendAutoAdd,
+		"users_setting_is_visible_friends":          User.UsersSettingIsVisibleFriends,
+		"users_setting_is_visible_statistics":       User.UsersSettingIsVisibleStatistics,
+		"users_setting_language":                    User.UsersSettingLanguage,
+		"users_setting_visibility_activity_summary": User.UsersSettingVisibilityActivitySummary,
+		"users_source":                              User.UsersSource,
+		"users_source_id":                           User.UsersSourceId,
+		"users_username":                            User.UsersUsername,
+		"users_breathing_points":                    breathingPoints,
+		"users_following_count":                     User.UsersFollowingCount,
+		"users_follower_count":                      User.UsersFollowerCount,
+		"users_friends_info":                        usersFriendsInfo,
+	}
+
+	// 如果有追蹤關係，添加到響應中
+	if User.UsersFollowings != nil {
+		response["users_followings"] = User.UsersFollowings
+	}
+
+	c.JSON(http.StatusOK, response)
 }
 
 func (r OosaUserRepository) ReadUserById(userId primitive.ObjectID, User *models.Users) error {
